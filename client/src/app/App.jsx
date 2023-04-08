c32e2c6d7c82928311197577e51244e13b4eafe2
takeru_tanaka <rhzxrzxk@gmail.com>
2023-04-08 16:23:18 +0900

    index.jsx の修正

---
+++ client/src/app/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div id="App">
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/material";
+import App from "./app/App";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StyledEngineProvider>
   </React.StrictMode>
 );
