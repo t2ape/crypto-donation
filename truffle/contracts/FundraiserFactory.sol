31673ecce843c858c3aeeb0fc1e76bdee70c4d9d
takeru_tanaka <rhzxrzxk@gmail.com>
2023-04-24 00:08:26 +0900

    スマートコントラクト側にバリデーションを設定

--- truffle/contracts/FundraiserFactory.sol
+++ truffle/contracts/FundraiserFactory.sol
@@ -14,7 +14,12 @@ contract FundraiserFactory {
     uint256 endedAt, // 任意
     address payable beneficiary // 必須
   ) public {
-    // TODO: 入力値のバリデーションを追加する
+    // validations
+    require(bytes(name).length > 0 && bytes(name).length <= 400, "name length is invalid.");
+    require(bytes(description).length > 0 && bytes(description).length <= 4000, "description length is invalid.");
+    require(bytes(url).length >= 0 && bytes(url).length <= 4000, "url length is invalid.");
+    require(bytes(imageUrl).length >= 0 && bytes(imageUrl).length <= 4000, "imageUrl length is invalid.");
+    require(beneficiary != address(0), "beneficiary format is invalid");
 
     Fundraiser fundraiser = new Fundraiser(
       name,
