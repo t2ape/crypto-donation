aafbca995087d46dff269fa31beafcd82efe1ae6
takeru_tanaka <rhzxrzxk@gmail.com>
2023-04-30 14:02:06 +0900

    ユーザー用の fundraisers 一覧を返す関数を作成

--- truffle/contracts/FundraiserFactory.sol
+++ truffle/contracts/FundraiserFactory.sol
@@ -6,6 +6,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract FundraiserFactory is Ownable {
   Fundraiser[] private _fundraisers;
 
+  function isActive(Fundraiser _fundraiser) private view returns (bool) {
+    return (
+      _fundraiser.isOpen() &&
+      _fundraiser.deletedAt() != 0 &&
+      _fundraiser.startedAt() <= block.timestamp &&
+      _fundraiser.endedAt() > block.timestamp
+    );
+  }
+
   function createFundraiser(
     string memory name, // 必須
     string memory description, // 必須
@@ -41,6 +50,40 @@ contract FundraiserFactory is Ownable {
 
   function fundraisersCount() public view returns (uint256) {
     return _fundraisers.length;
+  }
+
+  function activeFundraisersCount() public view returns (uint256) {
+    uint256 count = 0;
+    for (uint256 i = 0; i < _fundraisers.length; i++) {
+      if (isActive(_fundraisers[i])) { count++; }
+    }
+    return count;
+  }
+
+  function fundraisers_for_user(uint256 limit, uint256 offset) public view returns(Fundraiser[] memory collection) {
+    require(offset <= activeFundraisersCount(), "offset is over limit.");
+
+    uint256 size = activeFundraisersCount() - offset;
+    size = size < limit ? size : limit;
+    size = size < maxLimit ? size : maxLimit;
+    collection = new Fundraiser[](size);
+
+    uint256 collectionIndex = 0;
+
+    for (uint256 i = 0; i < _fundraisers.length; i++) {
+      if (isActive(_fundraisers[i])) {
+        if (collectionIndex >= offset && collectionIndex < offset + size) {
+          collection[collectionIndex - offset] = _fundraisers[i];
+        }
+        collectionIndex++;
+
+        if (collectionIndex >= offset + size) {
+          break;
+        }
+      }
+    }
+
+    return collection;
   }
 
   // TODO: アクセス制御をし全 fundraisers のリストは管理者だけがアクセスできるようにする
