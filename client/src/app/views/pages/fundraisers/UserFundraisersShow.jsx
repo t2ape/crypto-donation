8669279d77b6055697361f9d5dde18b10ae88125
takeru_tanaka <rhzxrzxk@gmail.com>
2023-05-14 02:25:07 +0900

    寄付についてフロント側の対応

--- client/src/app/views/pages/fundraisers/UserFundraisersShow.jsx
+++ client/src/app/views/pages/fundraisers/UserFundraisersShow.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {Button, Card, Divider, Grid, styled, useTheme} from "@mui/material";
+import {Button, Card, Divider, Grid, styled, TextField, useTheme} from "@mui/material";
 import FundraiserContract from "contracts/Fundraiser.json";
+import { Formik } from "formik";
 import {H4, Paragraph, Span} from "app/components/Typography";
 import getWeb3 from "utils/getWeb3";
 import {useNavigate, useParams} from "react-router-dom";
+import * as yup from "yup";
 
 // styled components
 const FundraiserIMG = styled(Card)({
@@ -19,9 +21,9 @@ const IMG = styled("img")({
   width: "100%",
 });
 
-const FundraiserShow = () => {
-  console.log('test')
+const Form = styled("form")({ paddingLeft: "16px", paddingRight: "16px" });
 
+const FundraiserShow = () => {
   const theme = useTheme();
   const primary = theme.palette.primary.main;
   const secondary = theme.palette.text.secondary;
@@ -40,6 +42,7 @@ const FundraiserShow = () => {
   const [ donationsAmount, setDonationsAmount ] = useState(null);
   const [ donationsCount, setDonationsCount ] = useState(null);
   const [ url, setUrl ] = useState(null);
+  const [ initialValues, setInitialValues ] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -77,6 +80,11 @@ const FundraiserShow = () => {
         setStartedAt(startedAt);
         const endedAt = await contract.methods.endedAt().call();
         setEndedAt(endedAt);
+
+        const initialValues = {
+          donationAmount: 0,
+        };
+        setInitialValues(initialValues);
       } catch(error) {
         alert(
           'Failed to load web3, accounts, or contract. Check console for details.',
@@ -86,6 +94,32 @@ const FundraiserShow = () => {
     }
     init();
   }, []);
+
+  const handleSubmit = (values) => {
+    const submitInputValues = async (values) => {
+      console.log(values);
+
+      const value = web3.utils.toWei(values.donationAmount.toString());
+
+      const gasLimit = await contract.methods.donate().estimateGas({
+        from: accounts[0],
+        value: value
+      });
+      const gasPrice = await web3.eth.getGasPrice();
+      await contract.methods.donate().send({
+        from: accounts[0],
+        value: value,
+        gasLimit,
+        gasPrice
+      });
+
+      alert('Successfully donated');
+
+      // TODO:JSON-RPC エラーが発生した時のために、例外をキャッチして alert を出す
+    }
+
+    submitInputValues(values);
+  }
 
   return (
     <Card sx={{ px: 4, py: 4 }} elevation={3}>
@@ -110,13 +144,35 @@ const FundraiserShow = () => {
             DonationsCount: {donationsCount}{"\n"}
           </Paragraph>
 
-          <Button type="submit" color="primary" variant="contained" onClick={() => {
-            if (window.confirm("Are you sure you want to donate? This cannot be undone.")) {
-              // deleteFundraiser();
-            }
-          }}>
-            Donate
-          </Button>
+          <Formik
+            onSubmit={handleSubmit}
+            enableReinitialize={true}
+            initialValues={initialValues ? initialValues : {
+              donationAmount: 0
+            }}
+            validationSchema={validationSchema}
+          >
+            {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+              <Form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center' }}>
+                <TextField
+                  type="text"
+                  name="donationAmount"
+                  label="Donation Amout"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.donationAmount || 0}
+                  error={Boolean(touched.donationAmount && errors.donationAmount)}
+                  helperText={touched.donationAmount && errors.donationAmount}
+                  style={{ marginRight: '10px' }}
+                />
+                <Button type="submit" color="primary" variant="contained" onClick={() => {
+                  if (window.confirm("Are you sure you want to donate? This cannot be undone.")) {}
+                }}>
+                  Donate
+                </Button>
+              </Form>
+            )}
+          </Formik>
 
           <Divider sx={{ my: 2 }} />
 
@@ -132,5 +188,13 @@ const FundraiserShow = () => {
     </Card>
   )
 }
+
+const validationSchema = yup.object().shape({
+  donationAmount: yup
+    .number()
+    .typeError('Donation Amount is number')
+    .positive('Donation Amount is greater than 0')
+    .required("Donation Amount is required")
+});
 
 export default FundraiserShow;
