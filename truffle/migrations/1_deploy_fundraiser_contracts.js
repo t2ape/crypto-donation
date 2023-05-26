bb9be83fb80ba9f0fd932949098fc75d5c2bf6a5
takeru_tanaka <rhzxrzxk@gmail.com>
2023-05-26 17:21:38 +0900

    fix

--- truffle/migrations/1_deploy_fundraiser_contracts.js
+++ truffle/migrations/1_deploy_fundraiser_contracts.js
@@ -1,11 +1,11 @@
 const FundraiserStorage = artifacts.require('FundraiserStorage');
-const AdminFundraiserLogic = artifacts.require('AdminFundraiserLogic');
+const AdministratorFundraiserHandler = artifacts.require('AdministratorFundraiserHandler');
 const UserFundraiserHandler = artifacts.require('UserFundraiserHandler');
 
 module.exports = function (deployer) {
   deployer.deploy(FundraiserStorage)
     .then(() => {
-      return deployer.deploy(AdminFundraiserLogic, FundraiserStorage.address);
+      return deployer.deploy(AdministratorFundraiserHandler, FundraiserStorage.address);
     })
     .then(() => {
       return deployer.deploy(UserFundraiserHandler, FundraiserStorage.address);
