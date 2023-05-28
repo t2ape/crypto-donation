203f7051bf5995c817b910629d32a22212c31ac5
takeru_tanaka <rhzxrzxk@gmail.com>
2023-05-28 16:22:13 +0900

    fix deploy script

--- truffle/migrations/1_deploy_fundraiser_contracts.js
+++ truffle/migrations/1_deploy_fundraiser_contracts.js
@@ -2,20 +2,16 @@ const FundraiserStorage = artifacts.require('FundraiserStorage');
 const AdministratorFundraiserHandler = artifacts.require('AdministratorFundraiserHandler');
 const UserFundraiserHandler = artifacts.require('UserFundraiserHandler');
 
-module.exports = function (deployer) {
-  deployer.deploy(FundraiserStorage).then(async () => {
-    const fundraiserStorageInstance = await FundraiserStorage.deployed();
+module.exports = async function (deployer) {
+  await deployer.deploy(FundraiserStorage);
+  const fundraiserStorageInstance = await FundraiserStorage.deployed();
 
-    deployer.deploy(AdministratorFundraiserHandler, fundraiserStorageInstance.address).then(async () => {
-      const adminFundraiserHandlerInstance = await AdministratorFundraiserHandler.deployed();
+  await deployer.deploy(AdministratorFundraiserHandler, fundraiserStorageInstance.address);
+  const adminFundraiserHandlerInstance = await AdministratorFundraiserHandler.deployed();
 
-      deployer.deploy(UserFundraiserHandler, fundraiserStorageInstance.address).then(async () => {
-        const userFundraiserHandlerInstance = await UserFundraiserHandler.deployed();
+  await deployer.deploy(UserFundraiserHandler, fundraiserStorageInstance.address);
+  const userFundraiserHandlerInstance = await UserFundraiserHandler.deployed();
 
-        await fundraiserStorageInstance.setAccessPermittedContract(adminFundraiserHandlerInstance.address);
-
-        await fundraiserStorageInstance.setAccessPermittedContract(userFundraiserHandlerInstance.address);
-      });
-    });
-  });
+  await fundraiserStorageInstance.setAccessPermittedContract(adminFundraiserHandlerInstance.address);
+  await fundraiserStorageInstance.setAccessPermittedContract(userFundraiserHandlerInstance.address);
 };
