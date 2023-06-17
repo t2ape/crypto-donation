7846a1d19e55747dfc984d014f0c275391ac22a4
takeru_tanaka <rhzxrzxk@gmail.com>
2023-06-18 08:29:13 +0900

    fix generate logic

--- truffle/contracts/tokens/heart_token/HeartToken.sol
+++ truffle/contracts/tokens/heart_token/HeartToken.sol
@@ -126,27 +126,88 @@ contract HeartToken is Ownable {
   }
 
   function _generateSVG(uint256 tokenId) internal pure returns (bytes memory) {
+    uint256[2] f_point_1 = [270, 45];
+    uint256[2] f_point_2 = [810, 45];
+    uint256[2] f_point_3 = [450, 90];
+    uint256[2] f_point_4 = [630, 90];
+    uint256[2] f_point_5 = [90, 180];
+    uint256[2] f_point_6 = [540, 180];
+    uint256[2] f_point_7 = [990, 180];
+    uint256[2] f_point_8 = [0, 360];
+    uint256[2] f_point_9 = [1080, 360];
+    uint256[2] f_point_10 = [90, 585];
+    uint256[2] f_point_11 = [990, 585];
+    uint256[2] f_point_12 = [180, 720];
+    uint256[2] f_point_13 = [900, 720];
+    uint256[2] f_point_14 = [360, 900];
+    uint256[2] f_point_15 = [720, 900];
+    uint256[2] f_point_16 = [540, 1080];
+
     uint256 nonce = 0;
-    uint256 seed = _randomValue(tokenId, nonce);
-    uint256[14] seeds = [
-      _randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 180,
-      _randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 180,
-      _randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 180,
-      _randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 180,
-      _randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 360,
-      _randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 360,
-      _randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 360,
-      _randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 360,
-      _randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 540,
-      _randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 540,
-      _randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 540,
-      _randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 540,
-      _randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 720,
-      _randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 720
+    uint256[2] r_point_1 = [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 180];
+    uint256[2] r_point_2 = [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 180];
+    uint256[2] r_point_3 = [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 180];
+    uint256[2] r_point_4 = [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 180];
+    uint256[2] r_point_5 = [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 360];
+    uint256[2] r_point_6 = [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 360];
+    uint256[2] r_point_7 = [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 360];
+    uint256[2] r_point_8 = [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 360];
+    uint256[2] r_point_9 = [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 540];
+    uint256[2] r_point_10 = [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 540];
+    uint256[2] r_point_11 = [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 540];
+    uint256[2] r_point_12 = [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 540];
+    uint256[2] r_point_13 = [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 720];
+    uint256[2] r_point_14 = [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 720];
+
+    uint256[2][3][42] memory seeds = [
+      [f_point_5, f_point_1, r_point_1], // uppermost part
+      [f_point_1, r_point_1, f_point_3], // uppermost part
+      [r_point_1, f_point_3, r_point_2], // uppermost part
+      [f_point_3, r_point_2, f_point_6], // uppermost part
+      [r_point_2, f_point_6, r_point_3], // uppermost part
+      [f_point_6, r_point_3, f_point_4], // uppermost part
+      [r_point_3, f_point_4, r_point_4], // uppermost part
+      [f_point_4, r_point_4, f_point_2], // uppermost part
+      [r_point_4, f_point_2, f_point_7], // uppermost part
+      [f_point_5, f_point_8, r_point_1], // upper left part
+      [f_point_8, r_point_1, r_point_5], // upper left part
+      [r_point_1, r_point_5, r_point_2], // upper left part
+      [r_point_5, r_point_2, r_point_6], // upper left part
+      [r_point_2, r_point_6, r_point_7], // upper left part
+      [r_point_2, r_point_7, r_point_3], // upper right part
+      [r_point_7, r_point_3, r_point_8], // upper right part
+      [r_point_3, r_point_8, r_point_4], // upper right part
+      [r_point_8, r_point_4, f_point_9], // upper right part
+      [r_point_4, f_point_9, f_point_7], // upper right part
+      [f_point_8, f_point_10, r_point_5], // middle left part
+      [f_point_10, r_point_5, f_point_12], // middle left part
+      [r_point_5, f_point_12, r_point_9], // middle left part
+      [f_point_12, r_point_9, r_point_13], // middle left part
+      [r_point_5, r_point_9, r_point_6], // middle center left part
+      [r_point_9, r_point_6, r_point_10], // middle center left part
+      [r_point_6, r_point_10, r_point_11], // middle center left part
+      [r_point_6, r_point_11, r_point_7], // middle center right part
+      [r_point_11, r_point_7, r_point_12], // middle center right part
+      [r_point_7, r_point_12, r_point_8], // middle center right part
+      [r_point_14, r_point_12, f_point_13], // middle right part
+      [r_point_12, f_point_13, r_point_8], // middle right part
+      [f_point_13, r_point_8, f_point_11], // middle right part
+      [r_point_8, f_point_11, f_point_9], // middle right part
+      [r_point_9, r_point_13, r_point_10], // lower part
+      [r_point_13, r_point_10, r_point_14], // lower part
+      [r_point_10, r_point_14, r_point_11], // lower part
+      [r_point_14, r_point_11, r_point_12], // lower part
+      [f_point_12, f_point_14, r_point_13], // lowest part
+      [f_point_14, r_point_13, f_point_16], // lowest part
+      [r_point_13, f_point_16, r_point_14], // lowest part
+      [f_point_16, r_point_14, f_point_15], // lowest part
+      [r_point_14, f_point_15, f_point_13] // lowest part
     ];
 
+    uint256 seed = _randomValue(tokenId, nonce++);
+    string[10] colors;
     if (seed % 10 == 0) {
-      string[10] colors = [
+      colors = [
         "#F94144",
         "#F3722C",
         "#F8961E",
@@ -159,7 +220,8 @@ contract HeartToken is Ownable {
         "#277DA1"
       ];
     } else {
-      string[6] colors = [
+      // TODO: 10 色にする?
+      colors = [
         "#E60033",
         "#E6B8C2",
         "#E68A9E",
@@ -169,7 +231,7 @@ contract HeartToken is Ownable {
       ];
     }
 
-    bytes memory image = _generateImage(seeds, colors);
+    bytes memory image = _generateImage(tokenId, seeds, colors);
 
     return abi.encodePacked(
       '<svg width="1080" height="1080" viewBox="0 0 1080 1080" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges">\n',
@@ -178,21 +240,21 @@ contract HeartToken is Ownable {
     );
   }
 
-  function _generateImage(uint256 tokenId, uint256[] _seeds, string[] _colors, uint256 _colorsLength) internal pure returns(bytes memory) {
+  function _generateImage(uint256 tokenId, uint256[2][3][42] _seeds, string[10] _colors) internal pure returns(bytes memory) {
     bytes memory pack;
     uint256 i;
-    for (i = 0; i < _seeds.length - 6; i++) {
-      uint256 _colorsIndex = _randomValue(tokenId, i) % _colorsLength;
+    for (i = 0; i < _seeds.length; i++) {
+      uint256 _colorsIndex = _randomValue(tokenId, i) % 10;
 
       pack = abi.encodePacked(
         pack,
         '<polygon points="',
-        _seeds[i], ' ', _seeds[i + 1], ', ',
-        _seeds[i + 2], ' ', _seeds[i + 3], ', ',
-        _seeds[i + 4], ' ', _seeds[i + 5], '"',
+        _seeds[i][0][0], ' ', _seeds[i][0][1], ', ',
+        _seeds[i][1][0], ' ', _seeds[i][1][1], ', ',
+        _seeds[i][2][0], ' ', _seeds[i][2][1], '" ',
         'fill="',
         _colors[_colorsIndex],
-        '/>\n'
+        '" />\n'
       );
     }
     return pack;
