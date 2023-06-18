4b2f0f6378bf64f66ff1bda3ff6ad61605c34121
takeru_tanaka <rhzxrzxk@gmail.com>
2023-06-18 18:05:43 +0900

    fix for compile

--- truffle/contracts/tokens/heart_token/HeartToken.sol
+++ truffle/contracts/tokens/heart_token/HeartToken.sol
@@ -5,12 +5,25 @@
 pragma solidity ^0.8.19;
 
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
-import { ERC721 } from './base/ERC721.sol';
+import { ERC721 } from '../../base/ERC721.sol';
 import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
-import { IProxyRegistry } from './external/opensea/IProxyRegistry.sol';
+//  TODO: opensea について調べたら proxyRegistry を用いる形に修正
+//import { IProxyRegistry } from '../../external/opensea/IProxyRegistry.sol';
+import "@openzeppelin/contracts/utils/Strings.sol";
+import { Base64 } from 'base64-sol/base64.sol';
 
-contract HeartToken is Ownable {
+contract HeartToken is Ownable, IERC721, ERC721 {
   using Strings for uint256;
+
+  event TokenCreated(uint256 indexed tokenId);
+
+  event TokenBurned(uint256 indexed tokenId);
+
+  event FoundersUpdated(address founders);
+
+  event MinterUpdated(address minter);
+
+  event MinterLocked();
 
   // The founders address (creators org)
   address public founders;
@@ -24,8 +37,9 @@ contract HeartToken is Ownable {
   // The internal token ID tracker
   uint256 private _currentTokenId;
 
-  // OpenSea's Proxy Registry
-  IProxyRegistry public immutable proxyRegistry;
+  //  TODO: opensea について調べたら proxyRegistry を用いる形に修正
+  //  // OpenSea's Proxy Registry
+  //  IProxyRegistry public immutable proxyRegistry;
 
   /**
    * @notice Require that the minter has not been locked.
@@ -83,17 +97,19 @@ contract HeartToken is Ownable {
    * @notice Mint a token to the minter, along with a possible founders reward
      * token. Founders reward tokens are minted every 10 token.
      */
-  function mint() public override onlyMinter {
+  function mint() public onlyMinter {
     if (_currentTokenId % 10 == 0) {
       _mint(owner(), founders, _currentTokenId++);
+      emit TokenCreated(_currentTokenId);
     }
     _mint(owner(), minter, _currentTokenId++);
+    emit TokenCreated(_currentTokenId);
   }
 
   /**
    * @notice Burn a token.
      */
-  function burn(uint256 tokenID) public override onlyMinter {
+  function burn(uint256 tokenID) public onlyMinter {
     _burn(tokenID);
     emit TokenBurned(tokenID);
   }
@@ -111,11 +127,11 @@ contract HeartToken is Ownable {
    * @notice Similar to `tokenURI`, but always serves a base64 encoded data URI
      * with the JSON contents directly inlined.
      */
-  function dataURI(uint256 tokenId) public view override returns (string memory) {
+  function dataURI(uint256 tokenId) public view returns (string memory) {
     require(_exists(tokenId), 'URI query for nonexistent token');
 
-    string memory tokenId = tokenId.toString();
-    string memory name = string(abi.encodePacked('Heart Token #', tokenId));
+    string memory tokenIdStringified = tokenId.toString();
+    string memory name = string(abi.encodePacked('Heart Token #', tokenIdStringified));
     string memory image = Base64.encode(_generateSVG(tokenId));
     return string(
       abi.encodePacked(
@@ -130,86 +146,90 @@ contract HeartToken is Ownable {
   }
 
   function _generateSVG(uint256 tokenId) internal pure returns (bytes memory) {
-    uint256[2] f_point_1 = [270, 45];
-    uint256[2] f_point_2 = [810, 45];
-    uint256[2] f_point_3 = [450, 90];
-    uint256[2] f_point_4 = [630, 90];
-    uint256[2] f_point_5 = [90, 180];
-    uint256[2] f_point_6 = [540, 180];
-    uint256[2] f_point_7 = [990, 180];
-    uint256[2] f_point_8 = [0, 360];
-    uint256[2] f_point_9 = [1080, 360];
-    uint256[2] f_point_10 = [90, 585];
-    uint256[2] f_point_11 = [990, 585];
-    uint256[2] f_point_12 = [180, 720];
-    uint256[2] f_point_13 = [900, 720];
-    uint256[2] f_point_14 = [360, 900];
-    uint256[2] f_point_15 = [720, 900];
-    uint256[2] f_point_16 = [540, 1080];
+    uint256[2][16] memory f_points = [
+      [uint256(270), uint256(45)],
+      [uint256(810), uint256(45)],
+      [uint256(450), uint256(90)],
+      [uint256(630), uint256(90)],
+      [uint256(90), uint256(180)],
+      [uint256(540), uint256(180)],
+      [uint256(990), uint256(180)],
+      [uint256(0), uint256(360)],
+      [uint256(1080), uint256(360)],
+      [uint256(90), uint256(585)],
+      [uint256(990), uint256(585)],
+      [uint256(180), uint256(720)],
+      [uint256(900), uint256(720)],
+      [uint256(360), uint256(900)],
+      [uint256(720), uint256(900)],
+      [uint256(540), uint256(1080)]
+    ];
 
     uint256 nonce = 0;
-    uint256[2] r_point_1 = [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 180];
-    uint256[2] r_point_2 = [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 180];
-    uint256[2] r_point_3 = [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 180];
-    uint256[2] r_point_4 = [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 180];
-    uint256[2] r_point_5 = [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 360];
-    uint256[2] r_point_6 = [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 360];
-    uint256[2] r_point_7 = [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 360];
-    uint256[2] r_point_8 = [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 360];
-    uint256[2] r_point_9 = [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 540];
-    uint256[2] r_point_10 = [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 540];
-    uint256[2] r_point_11 = [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 540];
-    uint256[2] r_point_12 = [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 540];
-    uint256[2] r_point_13 = [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 720];
-    uint256[2] r_point_14 = [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 720];
+    uint256[2][14] memory r_points = [
+      [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 180],
+      [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 180],
+      [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 180],
+      [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 180],
+      [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 360],
+      [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 360],
+      [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 360],
+      [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 360],
+      [_randomValue(tokenId, nonce++) % 180 + 180, _randomValue(tokenId, nonce++) % 180 + 540],
+      [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 540],
+      [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 540],
+      [_randomValue(tokenId, nonce++) % 180 + 720, _randomValue(tokenId, nonce++) % 180 + 540],
+      [_randomValue(tokenId, nonce++) % 180 + 360, _randomValue(tokenId, nonce++) % 180 + 720],
+      [_randomValue(tokenId, nonce++) % 180 + 540, _randomValue(tokenId, nonce++) % 180 + 720]
+    ];
 
     uint256[2][3][42] memory seeds = [
-      [f_point_5, f_point_1, r_point_1], // uppermost part
-      [f_point_1, r_point_1, f_point_3], // uppermost part
-      [r_point_1, f_point_3, r_point_2], // uppermost part
-      [f_point_3, r_point_2, f_point_6], // uppermost part
-      [r_point_2, f_point_6, r_point_3], // uppermost part
-      [f_point_6, r_point_3, f_point_4], // uppermost part
-      [r_point_3, f_point_4, r_point_4], // uppermost part
-      [f_point_4, r_point_4, f_point_2], // uppermost part
-      [r_point_4, f_point_2, f_point_7], // uppermost part
-      [f_point_5, f_point_8, r_point_1], // upper left part
-      [f_point_8, r_point_1, r_point_5], // upper left part
-      [r_point_1, r_point_5, r_point_2], // upper left part
-      [r_point_5, r_point_2, r_point_6], // upper left part
-      [r_point_2, r_point_6, r_point_7], // upper left part
-      [r_point_2, r_point_7, r_point_3], // upper right part
-      [r_point_7, r_point_3, r_point_8], // upper right part
-      [r_point_3, r_point_8, r_point_4], // upper right part
-      [r_point_8, r_point_4, f_point_9], // upper right part
-      [r_point_4, f_point_9, f_point_7], // upper right part
-      [f_point_8, f_point_10, r_point_5], // middle left part
-      [f_point_10, r_point_5, f_point_12], // middle left part
-      [r_point_5, f_point_12, r_point_9], // middle left part
-      [f_point_12, r_point_9, r_point_13], // middle left part
-      [r_point_5, r_point_9, r_point_6], // middle center left part
-      [r_point_9, r_point_6, r_point_10], // middle center left part
-      [r_point_6, r_point_10, r_point_11], // middle center left part
-      [r_point_6, r_point_11, r_point_7], // middle center right part
-      [r_point_11, r_point_7, r_point_12], // middle center right part
-      [r_point_7, r_point_12, r_point_8], // middle center right part
-      [r_point_14, r_point_12, f_point_13], // middle right part
-      [r_point_12, f_point_13, r_point_8], // middle right part
-      [f_point_13, r_point_8, f_point_11], // middle right part
-      [r_point_8, f_point_11, f_point_9], // middle right part
-      [r_point_9, r_point_13, r_point_10], // lower part
-      [r_point_13, r_point_10, r_point_14], // lower part
-      [r_point_10, r_point_14, r_point_11], // lower part
-      [r_point_14, r_point_11, r_point_12], // lower part
-      [f_point_12, f_point_14, r_point_13], // lowest part
-      [f_point_14, r_point_13, f_point_16], // lowest part
-      [r_point_13, f_point_16, r_point_14], // lowest part
-      [f_point_16, r_point_14, f_point_15], // lowest part
-      [r_point_14, f_point_15, f_point_13] // lowest part
+      [f_points[4], f_points[0], r_points[0]], // uppermost part
+      [f_points[0], r_points[0], f_points[2]], // uppermost part
+      [r_points[0], f_points[2], r_points[1]], // uppermost part
+      [f_points[2], r_points[1], f_points[5]], // uppermost part
+      [r_points[1], f_points[5], r_points[2]], // uppermost part
+      [f_points[5], r_points[2], f_points[3]], // uppermost part
+      [r_points[2], f_points[3], r_points[3]], // uppermost part
+      [f_points[3], r_points[3], f_points[1]], // uppermost part
+      [r_points[3], f_points[1], f_points[6]], // uppermost part
+      [f_points[4], f_points[7], r_points[0]], // upper left part
+      [f_points[7], r_points[0], r_points[4]], // upper left part
+      [r_points[0], r_points[4], r_points[1]], // upper left part
+      [r_points[4], r_points[1], r_points[5]], // upper left part
+      [r_points[1], r_points[5], r_points[6]], // upper left part
+      [r_points[1], r_points[6], r_points[2]], // upper right part
+      [r_points[6], r_points[2], r_points[7]], // upper right part
+      [r_points[2], r_points[7], r_points[3]], // upper right part
+      [r_points[7], r_points[3], f_points[8]], // upper right part
+      [r_points[3], f_points[8], f_points[6]], // upper right part
+      [f_points[7], f_points[9], r_points[4]], // middle left part
+      [f_points[9], r_points[4], f_points[11]], // middle left part
+      [r_points[4], f_points[11], r_points[8]], // middle left part
+      [f_points[11], r_points[8], r_points[12]], // middle left part
+      [r_points[4], r_points[8], r_points[5]], // middle center left part
+      [r_points[8], r_points[5], r_points[9]], // middle center left part
+      [r_points[5], r_points[9], r_points[10]], // middle center left part
+      [r_points[5], r_points[10], r_points[6]], // middle center right part
+      [r_points[10], r_points[6], r_points[11]], // middle center right part
+      [r_points[6], r_points[11], r_points[7]], // middle center right part
+      [r_points[13], r_points[11], f_points[12]], // middle right part
+      [r_points[11], f_points[12], r_points[7]], // middle right part
+      [f_points[12], r_points[7], f_points[10]], // middle right part
+      [r_points[7], f_points[10], f_points[8]], // middle right part
+      [r_points[8], r_points[12], r_points[9]], // lower part
+      [r_points[12], r_points[9], r_points[13]], // lower part
+      [r_points[9], r_points[13], r_points[10]], // lower part
+      [r_points[13], r_points[10], r_points[11]], // lower part
+      [f_points[11], f_points[13], r_points[12]], // lowest part
+      [f_points[13], r_points[12], f_points[15]], // lowest part
+      [r_points[12], f_points[15], r_points[13]], // lowest part
+      [f_points[15], r_points[13], f_points[14]], // lowest part
+      [r_points[13], f_points[14], f_points[12]] // lowest part
     ];
 
     uint256 seed = _randomValue(tokenId, nonce++);
-    string[10] colors;
+    string[10] memory colors;
     if (seed % 10 == 0) {
       colors = [
         "#F94144",
@@ -224,13 +244,16 @@ contract HeartToken is Ownable {
         "#277DA1"
       ];
     } else {
-      // TODO: 10 色にする?
       colors = [
-        "#E60033",
+        "#E6B8C2",
         "#E6B8C2",
         "#E68A9E",
+        "#E68A9E",
+        "#E65C7A",
         "#E65C7A",
         "#E62E56",
+        "#E62E56",
+        "#E60033",
         "#E60033"
       ];
     }
@@ -244,7 +267,7 @@ contract HeartToken is Ownable {
     );
   }
 
-  function _generateImage(uint256 tokenId, uint256[2][3][42] _seeds, string[10] _colors) internal pure returns(bytes memory) {
+  function _generateImage(uint256 tokenId, uint256[2][3][42] memory _seeds, string[10] memory _colors) internal pure returns(bytes memory) {
     bytes memory pack;
     uint256 i;
     for (i = 0; i < _seeds.length; i++) {
@@ -272,7 +295,7 @@ contract HeartToken is Ownable {
    * @notice Set the founders.
      * @dev Only callable by the founders when not locked.
      */
-  function setFounders(address _founders) external override onlyFounders {
+  function setFounders(address _founders) external onlyFounders {
     founders = _founders;
 
     emit FoundersUpdated(_founders);
@@ -282,7 +305,7 @@ contract HeartToken is Ownable {
    * @notice Set the token minter.
      * @dev Only callable by the owner when not locked.
      */
-  function setMinter(address _minter) external override onlyOwner whenMinterNotLocked {
+  function setMinter(address _minter) external onlyOwner whenMinterNotLocked {
     minter = _minter;
 
     emit MinterUpdated(_minter);
@@ -292,7 +315,7 @@ contract HeartToken is Ownable {
    * @notice Lock the minter.
      * @dev This cannot be reversed and is only callable by the owner when not locked.
      */
-  function lockMinter() external override onlyOwner whenMinterNotLocked {
+  function lockMinter() external onlyOwner whenMinterNotLocked {
     isMinterLocked = true;
 
     emit MinterLocked();
