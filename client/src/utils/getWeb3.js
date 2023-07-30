57cd69ad6e8934801886a53e15eb60fdb9503812
takeru_tanaka <rhzxrzxk@gmail.com>
2023-07-30 17:45:36 +0900

    fix getWeb3

--- client/src/utils/getWeb3.js
+++ client/src/utils/getWeb3.js
@@ -1,4 +1,4 @@
-import Web3 from "web3";
+import Web3 from 'web3';
 
 const getWeb3 = async () => {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
@@ -13,15 +13,15 @@ const getWeb3 = async () => {
   // Legacy dapp browsers...
   if (window.web3) {
     // Use Mist/MetaMask's provider.
-    const web3 = window.web3;
-    console.log("Injected web3 detected.");
+    const { web3 } = window;
+    console.log('Injected web3 detected.'); // eslint-disable-line no-console
     return web3;
   }
 
   // Fallback to localhost; use dev console port by default...
-  const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
+  const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
   const web3 = new Web3(provider);
-  console.log("No web3 instance injected, using Local web3.");
+  console.log('No web3 instance injected, using Local web3.'); // eslint-disable-line no-console
   return web3;
 };
 
