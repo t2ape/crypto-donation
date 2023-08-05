e51170020892d011506cf6f40c604b4811b24e4f
takeru_tanaka <rhzxrzxk@gmail.com>
2023-08-06 01:52:11 +0900

    fix

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+
 import { StyledEngineProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './app/App';
 
 // third party css
