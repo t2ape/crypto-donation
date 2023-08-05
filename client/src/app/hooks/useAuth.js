deb512122646d97bc5a8a434e21c4d3eae30421b
takeru_tanaka <rhzxrzxk@gmail.com>
2023-08-06 01:44:24 +0900

    不要な hooks を削除

--- client/src/app/hooks/useAuth.js
+++
@@ -1,8 +0,0 @@
-// import AuthContext from "app/contexts/JWTAuthContext";
-import AuthContext from "app/contexts/FirebaseAuthContext";
-// import AuthContext from "app/contexts/Auth0Context";
-import { useContext } from "react";
-
-const useAuth = () => useContext(AuthContext);
-
-export default useAuth;
