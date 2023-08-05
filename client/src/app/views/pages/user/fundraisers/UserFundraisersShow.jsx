c047506aeace4b31403e18f0803666147de6f3fd
takeru_tanaka <rhzxrzxk@gmail.com>
2023-08-05 23:48:08 +0900

    fix UserFundraisersShow

--- client/src/app/views/pages/user/fundraisers/UserFundraisersShow.jsx
+++ client/src/app/views/pages/user/fundraisers/UserFundraisersShow.jsx
@@ -1,200 +1,241 @@
-import React, { useEffect, useState } from "react";
-import {Button, Card, Divider, Grid, styled, TextField, useTheme} from "@mui/material";
-import FundraiserContract from "contracts/Fundraiser.json";
-import { Formik } from "formik";
-import {H4, Paragraph, Span} from "app/components/Typography";
-import getWeb3 from "utils/getWeb3";
-import {useNavigate, useParams} from "react-router-dom";
-import * as yup from "yup";
+import React, { useEffect, useState } from 'react';
+
+import {
+  Button, Card, Divider, Grid, styled, TextField,
+} from '@mui/material';
+import { Formik } from 'formik';
+import { useParams } from 'react-router-dom';
+import * as yup from 'yup';
+
+import { Breadcrumb } from 'app/components';
+import { H4, Paragraph } from 'app/components/Typography';
+import FundraiserContract from 'contracts/Fundraiser.json';
+import getWeb3 from 'utils/getWeb3';
 
 // styled components
+const Container = styled('div')(({ theme }) => ({
+  margin: '30px',
+  [theme.breakpoints.down('sm')]: { margin: '16px' },
+  '& .breadcrumb': {
+    marginBottom: '30px',
+    [theme.breakpoints.down('sm')]: { marginBottom: '16px' },
+  },
+}));
 const FundraiserIMG = styled(Card)({
-  display: "flex",
-  alignItems: "center",
-  flexDirection: "column",
-  justifyContent: "center",
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  justifyContent: 'center',
 });
-
-const IMG = styled("img")({
+const IMG = styled('img')({
   padding: 32,
   maxWidth: 400,
-  width: "100%",
+  width: '100%',
 });
+const StyledTextField = styled(TextField)({ marginBottom: '16px' });
+const Form = styled('form')({ paddingLeft: '16px', paddingRight: '16px' });
 
-const Form = styled("form")({ paddingLeft: "16px", paddingRight: "16px" });
-
-const FundraiserShow = () => {
-  const theme = useTheme();
-  const primary = theme.palette.primary.main;
-  const secondary = theme.palette.text.secondary;
+function FundraiserShow() {
   const { id } = useParams();
-  const [ web3, setWeb3 ] = useState(null);
-  const [ contract, setContract ] = useState(null);
-  const [ accounts, setAccounts ] = useState(null);
-  const [ name, setName ] = useState(null);
-  const [ description, setDescription ] = useState(null);
-  const [ imageUrl, setImageUrl ] = useState(null);
-  const [ beneficiary, setBeneficiary ] = useState(null);
-  const [ isOpen, setIsOpen ] = useState(null);
-  const [ startedAt, setStartedAt ] = useState(null);
-  const [ endedAt, setEndedAt ] = useState(null);
-  const [ deletedAt, setDeletedAt ] = useState(null);
-  const [ donationsAmount, setDonationsAmount ] = useState(null);
-  const [ donationsCount, setDonationsCount ] = useState(null);
-  const [ url, setUrl ] = useState(null);
-  const [ initialValues, setInitialValues ] = useState(null);
-  const navigate = useNavigate();
+  const [web3, setWeb3] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [accounts, setAccounts] = useState(null);
+  const [name, setName] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [startedAt, setStartedAt] = useState(null);
+  const [endedAt, setEndedAt] = useState(null);
+  const [donationsAmountLabel, setDonationsAmountLabel] = useState(null);
+  const [donationsCount, setDonationsCount] = useState(null);
+  const [url, setUrl] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const details = [
+    {
+      label: 'StartedAt:',
+      value: startedAt ? new Date(startedAt * 1000).toLocaleString() : null,
+    },
+    {
+      label: 'EndedAt:',
+      value: endedAt ? new Date(endedAt * 1000).toLocaleString() : null,
+    },
+    { label: 'DonationsAmount:', value: donationsAmountLabel },
+    { label: 'DonationsCount:', value: donationsCount },
+    { label: 'URL:', value: url },
+  ];
 
   useEffect(() => {
     const init = async () => {
       try {
-        const web3 = await getWeb3();
-        setWeb3(web3);
-        const contract = new web3.eth.Contract(
+        const localWeb3 = await getWeb3();
+        setWeb3(localWeb3);
+        const localContract = new localWeb3.eth.Contract(
           FundraiserContract.abi,
           id,
         );
-        setContract(contract);
-        const accounts = await web3.eth.getAccounts();
-        setAccounts(accounts);
+        setContract(localContract);
+        const localAccounts = await localWeb3.eth.getAccounts();
+        setAccounts(localAccounts);
 
-        const name = await contract.methods.name().call();
-        setName(name);
-        const description = await contract.methods.description().call();
-        setDescription(description);
-        const url = await contract.methods.url().call();
-        setUrl(url);
-        const imageUrl = await contract.methods.imageUrl().call();
-        setImageUrl(imageUrl);
-        const beneficiary = await contract.methods.beneficiary().call();
-        setBeneficiary(beneficiary);
-        const isOpen = await contract.methods.isOpen().call();
-        setIsOpen(isOpen);
-        const donationsAmount = await contract.methods.donationsAmount().call();
-        setDonationsAmount(donationsAmount);
-        const donationsCount = await contract.methods.donationsCount().call();
-        setDonationsCount(donationsCount);
-        const deletedAt = await contract.methods.deletedAt().call();
-        setDeletedAt(deletedAt);
-        const startedAt = await contract.methods.startedAt().call();
-        setStartedAt(startedAt);
-        const endedAt = await contract.methods.endedAt().call();
-        setEndedAt(endedAt);
-
-        const initialValues = {
-          donationAmount: 0,
-        };
-        setInitialValues(initialValues);
-      } catch(error) {
+        const localName = await localContract.methods.name().call();
+        setName(localName);
+        const localDescription = await localContract.methods
+          .description()
+          .call();
+        setDescription(localDescription);
+        const localUrl = await localContract.methods.url().call();
+        setUrl(localUrl);
+        const localImageUrl = await localContract.methods.imageUrl().call();
+        setImageUrl(localImageUrl);
+        const localDonationsAmount = await localContract.methods
+          .donationsAmount()
+          .call();
+        const localDonationsAmountLabel = localWeb3.utils.fromWei(
+          localDonationsAmount,
+          'ether',
+        );
+        setDonationsAmountLabel(localDonationsAmountLabel);
+        const localDonationsCount = await localContract.methods
+          .donationsCount()
+          .call();
+        setDonationsCount(localDonationsCount);
+        const localStartedAt = await localContract.methods.startedAt().call();
+        setStartedAt(localStartedAt);
+        const localEndedAt = await localContract.methods.endedAt().call();
+        setEndedAt(localEndedAt);
+      } catch (error) {
         alert(
           'Failed to load web3, accounts, or contract. Check console for details.',
         );
-        console.error(error);
+        console.error(error); // eslint-disable-line no-console
       }
-    }
+    };
     init();
   }, []);
 
-  const handleSubmit = (values) => {
-    const submitInputValues = async (values) => {
-      console.log(values);
+  const handleFormSubmit = (values) => {
+    const submitInputValues = async (inputValues) => {
+      try {
+        const value = web3.utils.toWei(inputValues.donationAmount.toString());
 
-      const value = web3.utils.toWei(values.donationAmount.toString());
+        const gasLimit = await contract.methods.donate().estimateGas({
+          from: accounts[0],
+          value,
+        });
+        const gasPrice = await web3.eth.getGasPrice();
+        await contract.methods.donate().send({
+          from: accounts[0],
+          value,
+          gasLimit,
+          gasPrice,
+        });
 
-      const gasLimit = await contract.methods.donate().estimateGas({
-        from: accounts[0],
-        value: value
-      });
-      const gasPrice = await web3.eth.getGasPrice();
-      await contract.methods.donate().send({
-        from: accounts[0],
-        value: value,
-        gasLimit,
-        gasPrice
-      });
+        alert('Successfully donated');
+      } catch (error) {
+        console.error(error); // eslint-disable-line no-console
+        alert('Failed to create fundraiser');
+      }
+    };
 
-      alert('Successfully donated');
-
-      // TODO:JSON-RPC エラーが発生した時のために、例外をキャッチして alert を出す
+    if (
+      window.confirm('Are you sure you want to donate? This cannot be undone.')
+    ) {
+      submitInputValues(values);
     }
+  };
 
-    submitInputValues(values);
-  }
+  const initialValues = {
+    donationAmount: 0,
+  };
+
+  const validationSchema = yup.object().shape({
+    donationAmount: yup
+      .number()
+      .typeError('Donation amount should be a number')
+      .required('Donation amount is required')
+      .moreThan(0, 'Donation amount should be greater than 0'),
+  });
 
   return (
-    <Card sx={{ px: 4, py: 4 }} elevation={3}>
-      <Grid container spacing={3}>
-        <Grid item md={6} xs={12}>
-          <FundraiserIMG>
-            <IMG src="https://fastly.picsum.photos/id/436/200/300.jpg?hmac=OuJRsPTZRaNZhIyVFbzDkMYMyORVpV86q5M8igEfM3Y" />
-          </FundraiserIMG>
+    <Container>
+      <div className="breadcrumb">
+        <Breadcrumb
+          routeSegments={[
+            { name: 'Fundraisers', path: '/user/fundraisers' },
+            { name: 'Donate Fundraiser' },
+          ]}
+        />
+      </div>
+
+      <Card sx={{ px: 4, py: 4 }} elevation={3}>
+        <Grid container spacing={3}>
+          <Grid item md={6} xs={12}>
+            <FundraiserIMG>
+              <IMG src={imageUrl} />
+            </FundraiserIMG>
+          </Grid>
+
+          <Grid item md={6} xs={12}>
+            <H4 sx={{ mt: 0, mb: 2 }}>{name}</H4>
+
+            <p>{description}</p>
+
+            <Divider sx={{ my: 2 }} />
+
+            <Paragraph sx={{ mt: 0, mb: 1 }} style={{ whiteSpace: 'pre-line' }}>
+              {details.map((detail) => (
+                <React.Fragment key={detail.label}>
+                  {detail.label}
+                  {' '}
+                  {detail.value}
+                  {'\n'}
+                </React.Fragment>
+              ))}
+            </Paragraph>
+
+            <Divider sx={{ my: 2 }} />
+
+            <Formik
+              onSubmit={handleFormSubmit}
+              enableReinitialize
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+              }) => (
+                <Form
+                  onSubmit={handleSubmit}
+                  style={{ display: 'flex', alignItems: 'center' }}
+                >
+                  <StyledTextField
+                    type="text"
+                    name="donationAmount"
+                    label="Donation Amout (ether)"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.donationAmount || 0}
+                    error={Boolean(
+                      touched.donationAmount && errors.donationAmount,
+                    )}
+                    helperText={touched.donationAmount && errors.donationAmount}
+                    style={{ marginRight: '20px' }}
+                  />
+                  <Button type="submit" color="primary" variant="contained">
+                    Donate
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Grid>
         </Grid>
-
-        <Grid item md={6} xs={12}>
-          <H4 sx={{ mt: 0, mb: 2 }}>{name}</H4>
-          <Paragraph sx={{ mt: 0, mb: 1, color: secondary }} style={{ whiteSpace: "pre-line" }}>
-            StartedAt: {startedAt}{"\n"}
-            EndedAt: {endedAt}{"\n"}
-          </Paragraph>
-
-          <Divider sx={{ my: 2 }} />
-
-          <Paragraph sx={{ mt: 0, mb: 1, color: secondary }} style={{ whiteSpace: "pre-line" }}>
-            DonationsAmount: {donationsAmount}{"\n"}
-            DonationsCount: {donationsCount}{"\n"}
-          </Paragraph>
-
-          <Formik
-            onSubmit={handleSubmit}
-            enableReinitialize={true}
-            initialValues={initialValues ? initialValues : {
-              donationAmount: 0
-            }}
-            validationSchema={validationSchema}
-          >
-            {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
-              <Form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center' }}>
-                <TextField
-                  type="text"
-                  name="donationAmount"
-                  label="Donation Amout"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  value={values.donationAmount || 0}
-                  error={Boolean(touched.donationAmount && errors.donationAmount)}
-                  helperText={touched.donationAmount && errors.donationAmount}
-                  style={{ marginRight: '10px' }}
-                />
-                <Button type="submit" color="primary" variant="contained" onClick={() => {
-                  if (window.confirm("Are you sure you want to donate? This cannot be undone.")) {}
-                }}>
-                  Donate
-                </Button>
-              </Form>
-            )}
-          </Formik>
-
-          <Divider sx={{ my: 2 }} />
-
-          <Paragraph sx={{ mt: 0, mb: 1, color: secondary }} style={{ whiteSpace: "pre-line" }}>
-            Url: {url}
-          </Paragraph>
-
-          <Divider sx={{ my: 2 }} />
-
-          <p>{description}</p>
-        </Grid>
-      </Grid>
-    </Card>
-  )
+      </Card>
+    </Container>
+  );
 }
-
-const validationSchema = yup.object().shape({
-  donationAmount: yup
-    .number()
-    .typeError('Donation Amount is number')
-    .positive('Donation Amount is greater than 0')
-    .required("Donation Amount is required")
-});
 
 export default FundraiserShow;
