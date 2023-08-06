480742648b4b88bc62b2a62bd6d8be09a7988d13
takeru_tanaka <rhzxrzxk@gmail.com>
2023-08-06 10:38:20 +0900

    autofix truffle/contracts/FundraiserStorage.sol

--- truffle/contracts/FundraiserStorage.sol
+++ truffle/contracts/FundraiserStorage.sol
@@ -20,9 +20,9 @@ contract FundraiserStorage is Ownable {
     _;
   }
 
-  function msgSenderIsAccessPermittedContract() private view returns(bool) {
-    for(uint256 i = 0; i < _accessPermittedContracts.length; i++) {
-      if(_accessPermittedContracts[i] == msg.sender) {
+  function msgSenderIsAccessPermittedContract() private view returns (bool) {
+    for (uint256 i = 0; i < _accessPermittedContracts.length; i++) {
+      if (_accessPermittedContracts[i] == msg.sender) {
         return true;
       }
     }
@@ -35,8 +35,8 @@ contract FundraiserStorage is Ownable {
   }
 
   function removeAccessPermittedContract(address _contract) public onlyOwner {
-    for(uint256 i = 0; i < _accessPermittedContracts.length; i++) {
-      if(_accessPermittedContracts[i] == _contract) {
+    for (uint256 i = 0; i < _accessPermittedContracts.length; i++) {
+      if (_accessPermittedContracts[i] == _contract) {
         // Delete does not change the array length.
         // It resets the value at index to it's default value,
         // in this case address(0)
@@ -47,91 +47,114 @@ contract FundraiserStorage is Ownable {
 
   // *** Getter Methods ***
 
-  function getAddress(bytes32 _key) external view returns(address) {
+  function getAddress(bytes32 _key) external view returns (address) {
     return addressStorage[_key];
   }
 
-  function getAddressArray(bytes32 _key) external view returns(address[] memory) {
+  function getAddressArray(
+    bytes32 _key
+  ) external view returns (address[] memory) {
     return addressArrayStorage[_key];
   }
 
-  function getBool(bytes32 _key) external view returns(bool) {
+  function getBool(bytes32 _key) external view returns (bool) {
     return boolStorage[_key];
   }
 
-  function getBytes(bytes32 _key) external view returns(bytes memory) {
+  function getBytes(bytes32 _key) external view returns (bytes memory) {
     return bytesStorage[_key];
   }
 
-  function getInt(bytes32 _key) external view returns(int) {
+  function getInt(bytes32 _key) external view returns (int) {
     return intStorage[_key];
   }
 
-  function getUint(bytes32 _key) external view returns(uint) {
+  function getUint(bytes32 _key) external view returns (uint) {
     return uintStorage[_key];
   }
 
-  function getString(bytes32 _key) external view returns(string memory) {
+  function getString(bytes32 _key) external view returns (string memory) {
     return stringStorage[_key];
   }
 
   // *** Setter Methods ***
 
-  function setAddress(bytes32 _key, address _value) onlyAccessPermittedContract external {
+  function setAddress(
+    bytes32 _key,
+    address _value
+  ) external onlyAccessPermittedContract {
     addressStorage[_key] = _value;
   }
 
-  function setAddressArray(bytes32 _key, address[] memory _values) onlyAccessPermittedContract external {
+  function setAddressArray(
+    bytes32 _key,
+    address[] memory _values
+  ) external onlyAccessPermittedContract {
     addressArrayStorage[_key] = _values;
   }
 
-  function setBool(bytes32 _key, bool _value) onlyAccessPermittedContract external {
+  function setBool(
+    bytes32 _key,
+    bool _value
+  ) external onlyAccessPermittedContract {
     boolStorage[_key] = _value;
   }
 
-  function setBytes(bytes32 _key, bytes calldata _value) onlyAccessPermittedContract external {
+  function setBytes(
+    bytes32 _key,
+    bytes calldata _value
+  ) external onlyAccessPermittedContract {
     bytesStorage[_key] = _value;
   }
 
-  function setInt(bytes32 _key, int _value) onlyAccessPermittedContract external {
+  function setInt(
+    bytes32 _key,
+    int _value
+  ) external onlyAccessPermittedContract {
     intStorage[_key] = _value;
   }
 
-  function setUint(bytes32 _key, uint _value) onlyAccessPermittedContract external {
+  function setUint(
+    bytes32 _key,
+    uint _value
+  ) external onlyAccessPermittedContract {
     uintStorage[_key] = _value;
   }
 
-  function setString(bytes32 _key, string calldata _value) onlyAccessPermittedContract external {
+  function setString(
+    bytes32 _key,
+    string calldata _value
+  ) external onlyAccessPermittedContract {
     stringStorage[_key] = _value;
   }
 
   // *** Delete Methods ***
 
-  function deleteAddress(bytes32 _key) onlyAccessPermittedContract external {
+  function deleteAddress(bytes32 _key) external onlyAccessPermittedContract {
     delete addressStorage[_key];
   }
 
-  function deleteAddress(bytes32 _key) onlyAccessPermittedContract external {
+  function deleteAddress(bytes32 _key) external onlyAccessPermittedContract {
     delete addressArrayStorage[_key];
   }
 
-  function deleteBool(bytes32 _key) onlyAccessPermittedContract external {
+  function deleteBool(bytes32 _key) external onlyAccessPermittedContract {
     delete boolStorage[_key];
   }
 
-  function deleteBytes(bytes32 _key) onlyAccessPermittedContract external {
+  function deleteBytes(bytes32 _key) external onlyAccessPermittedContract {
     delete bytesStorage[_key];
   }
 
-  function deleteInt(bytes32 _key) onlyAccessPermittedContract external {
+  function deleteInt(bytes32 _key) external onlyAccessPermittedContract {
     delete intStorage[_key];
   }
 
-  function deleteUint(bytes32 _key) onlyAccessPermittedContract external {
+  function deleteUint(bytes32 _key) external onlyAccessPermittedContract {
     delete uintStorage[_key];
   }
 
-  function deleteString(bytes32 _key) onlyAccessPermittedContract external {
+  function deleteString(bytes32 _key) external onlyAccessPermittedContract {
     delete stringStorage[_key];
   }
 }
